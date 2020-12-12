77f2b7cc7246c44fa355bbff7c3d1c1631bd0d6b
Ewan Brain <ewan.brain@gmail.com>
2020-12-12 12:54:47 +0000

    pokemon_center.sql tentatively set up, will need edits later

---
+++ console.py
@@ -0,0 +1,9 @@
+from models.pokemon import Pokemon
+from models.trainer import Trainer
+from models.nurse import Nurse
+
+import repositories.pokemon_repository as pokemon_repository
+import repositories.trainer_repository as trainer_repository
+import repositories.nurse_repository as nurse_repository
+
+pokemon_1 = Pokemon('Sparky', 'Ritchie', 'Pikachu', '15/6/2017')
---
+++ db/pokemon_center.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS pokemons;
+DROP TABLE IF EXISTS trainers;
+DROP TABLE IF EXISTS nurses;
+
+CREATE TABLE nurses (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+
+);
+
+CREATE TABLE trainers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    pokenav INT,
+);
+
+CREATE TABLE pokemons (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    trainer_id INT REFERENCES trainers(id) ON DELETE CASCADE,
+    nurse_id INT REFERENCES nurses(id) ON DELETE CASCADE, --> come back to this, it might not be implemented correctly
+    species VARCHAR(255),
+    hatched VARCHAR(255), --> potentially change for extensions
+
+);
